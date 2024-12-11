353d4e405d21e6961192b7bf599a5af59dd387e2
ariel3142 <beltsazarariel5@gmail.com>
2024-12-11 11:43:24 +0700

    first commit

---
+++ lib/login_page.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:water_drop_nav_bar/water_drop_nav_bar.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  Future<void> login(String email, String password) async {
+    setState(() {
+      _isLoading = true;
+      _errorMessage = null;
+    });
+
+    final url = Uri.parse('https://reqres.in/api/login');
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'email': email, 'password': password}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final token = data['token'];
+        print('Login successful: $token');
+        // Redirect to the next page
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const MyHomePage()),
+        );
+      } else {
+        final errorData = json.decode(response.body);
+        setState(() {
+          _errorMessage = errorData['error'];
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'An error occurred. Please try again later.';
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            if (_isLoading)
+              CircularProgressIndicator()
+            else
+              ElevatedButton(
+                onPressed: () {
+                  final email = _emailController.text.trim();
+                  final password = _passwordController.text.trim();
+                  if (email.isNotEmpty && password.isNotEmpty) {
+                    login(email, password);
+                  } else {
+                    setState(() {
+                      _errorMessage = 'Please fill in all fields';
+                    });
+                  }
+                },
+                child: Text('Login'),
+              ),
+            if (_errorMessage != null) ...[
+              SizedBox(height: 20),
+              Text(
+                _errorMessage!,
+                style: TextStyle(color: Colors.red),
+              ),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Color navigationBarColor = const Color.fromARGB(255, 75, 3, 244);
+  int selectedIndex = 0;
+  late PageController pageController;
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: selectedIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        systemNavigationBarColor: Colors.white,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+      child: Scaffold(
+        body: PageView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: pageController,
+          children: <Widget>[
+            Center(
+              child: Text(
+                'Bookmark Page',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Favorites Page',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Comments Section',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Feel free to leave a comment!',
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Text(
+                'Files Page',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: WaterDropNavBar(
+          backgroundColor: navigationBarColor,
+          onItemSelected: (int index) {
+            setState(() {
+              selectedIndex = index;
+            });
+            pageController.animateToPage(selectedIndex,
+                duration: const Duration(milliseconds: 400),
+                curve: Curves.easeOutQuad);
+          },
+          selectedIndex: selectedIndex,
+          barItems: <BarItem>[
+            BarItem(
+              filledIcon: Icons.bookmark_rounded,
+              outlinedIcon: Icons.bookmark_border_rounded,
+            ),
+            BarItem(
+              filledIcon: Icons.favorite_rounded,
+              outlinedIcon: Icons.favorite_border_rounded,
+            ),
+            BarItem(
+              filledIcon: Icons.comment_rounded,
+              outlinedIcon: Icons.comment_bank_rounded,
+            ),
+            BarItem(
+              filledIcon: Icons.folder_rounded,
+              outlinedIcon: Icons.folder_outlined,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
